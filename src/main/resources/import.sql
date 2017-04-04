3ef28f8b1fd9f1b25e92245c5a1cfc147b5d531c
gwo <g.wolf@online.de>
2017-04-04 08:29:22 +0200

    Add Comment to import.sql file

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,5 @@
 -- Insert tasks
+-- inserts initial data into the database when starting the application
 insert into task (complete,description) values (true,'Code Task entity');
 insert into task (complete,description) values (false,'Discuss users and roles');
 insert into task (complete,description) values (false,'Enable Spring Security');
