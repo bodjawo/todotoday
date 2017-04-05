ec12142de2744f26b03ffdb77716b5ea23493f5c
gwo <g.wolf@online.de>
2017-04-05 08:49:11 +0200

    Spring security with java config

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,10 +1,10 @@
 -- Insert role
-insert into role (name) values ('ROLE_USER')
+insert into role (name) values ('ROLE_USER');
 
 -- Insert users
 -- 1 = role id
-insert into user (username,enabled,password,role_id) values ('gottlieb_wolf',true,'password',1)
-insert into user (username,enabled,password,role_id) values ('andy_wolf',true,'password',1)
+insert into user (username,enabled,password,role_id) values ('gottlieb_wolf',true,'password',1);
+insert into user (username,enabled,password,role_id) values ('andy_wolf',true,'password',1);
 
 -- Insert tasks
 -- inserts initial data into the database when starting the application
